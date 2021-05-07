111c715abfd1aa1ee3e9a926ef945536e6fe300b
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-08 01:39:00 +0530

    Add `safe-area-inset` padding

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,13 @@
     <router-view/>
   </div>
 </template>
+
+<style scoped lang="css">
+  #app-root {
+    padding:
+      max(env(safe-area-inset-top), 1em)
+      max(env(safe-area-inset-right), 1em)
+      max(env(safe-area-inset-bottom), 1em)
+      max(env(safe-area-inset-left), 1em);
+  }
+</style>
