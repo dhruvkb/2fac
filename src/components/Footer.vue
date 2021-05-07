88eeba08132befd07b14d8badeff5afa826ded6c
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-08 03:59:00 +0530

    Create the header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer text-sm text-gray-500 p-2 border-t border-gray-200">
+    An experiment by
+    <a
+      class="font-bold text-black hover:underline"
+      href="https://dhruvkb.github.io/">
+      @dhruvkb</a>.
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="flex border-b border-gray-200">
+    <div class="site-title mr-auto p-2">
+      <h1 class="flex items-center font-bold text-2xl">
+        <ion-icon name="id-card-outline"/>
+        <RouterLink
+          class="ml-1"
+          :to="{ name: 'home'}">
+          Multifac
+        </RouterLink>
+      </h1>
+    </div>
+    <nav class="grid grid-cols-2 self-stretch">
+      <RouterLink
+        class="flex items-end justify-center text-gray-500 hover:text-black px-2 border-b-2 border-transparent"
+        v-for="(link, index) in links"
+        :key="index"
+        :to="{ name: link.toName }"
+        active-class="text-black border-black">
+        <div class="pb-2">{{ link.text }}</div>
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Header',
+    data() {
+      return {
+        links: [
+          {
+            text: 'Integration',
+            toName: 'integration',
+          },
+          {
+            text: 'About',
+            toName: 'about',
+          },
+        ],
+      }
+    },
+  })
+</script>
