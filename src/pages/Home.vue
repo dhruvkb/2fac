c089812d11daac82f9f7f7623dfb158e35972cc0
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-09 11:21:51 +0530

    Show a message when no accounts added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="page" id="home">
-    <Timeline/>
-    <div class="grid gap-1 tp:gap-4 grid-cols-2 tl:grid-cols-3 dr:grid-cols-4 p-2 my-4">
-      <Card
-        v-for="(acc,index) in accounts"
-        :key="index"
-        :account="acc"
-        :is-active="index === 5"/>
-    </div>
+    <template v-if="accounts.length">
+      <Timeline/>
+      <div class="grid gap-1 tp:gap-4 grid-cols-2 tl:grid-cols-3 dr:grid-cols-4 p-2 my-8">
+        <Card
+          v-for="(acc,index) in accounts"
+          :key="index"
+          :account="acc"
+          :is-active="index === 5"/>
+      </div>
+    </template>
+    <template v-else>
+      <div class="text-center p-2 my-8">
+        It's a little lonely in here.
+      </div>
+    </template>
   </div>
 </template>
 
