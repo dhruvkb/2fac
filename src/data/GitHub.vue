50b1851920ec25ab0faa49d02d7adcaf88669c2a
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-09 22:56:12 +0530

    Add functionality to read from and write to GitHub

--- src/data/GitHub.vue
+++ src/data/GitHub.vue
@@ -23,57 +23,210 @@
         class="button text-blue-900 bg-blue-100 hover:bg-blue-200 focus:ring-blue-500"
         :disabled="!accessToken"
         @click="validateToken">
-        Validate
+        Log in
       </button>
       <span
-        v-if="outcome"
+        v-if="loginOutcome"
         class="ml-2"
-        :class="outcome.isSuccessful ? 'text-green-600' : 'text-red-600'">
-        {{ outcome.message }}
+        :class="loginOutcome.isSuccessful ? 'text-green-600' : 'text-red-600'">
+        {{ loginOutcome.message }}
       </span>
+    </div>
+
+    <div class="grid tl:grid-cols-2 gap-2 mt-4">
+      <div class="tl:pr-2">
+        <h3 class="font-bold text-2xl">Import</h3>
+        <p class="mt-4" v-if="username">
+          Will read
+          <code class="text-sm bg-gray-100 p-1 rounded-md">{{ fileName }}</code>
+          from
+          <code class="text-sm bg-gray-100 p-1 rounded-md">{{ username }}/{{ repoName }}</code>.
+        </p>
+        <div class="action mt-4">
+          <button
+            class="button text-blue-900 bg-blue-100 hover:bg-blue-200 focus:ring-blue-500"
+            :disabled="!username"
+            @click="readFromGitHub">
+            Import
+          </button>
+          <span
+            v-if="readOutcome"
+            class="ml-2"
+            :class="readOutcome.isSuccessful ? 'text-green-600' : 'text-red-600'">
+            {{ readOutcome.message }}
+          </span>
+        </div>
+      </div>
+
+      <div class="tl:pl-2 tl:border-l border-gray-200">
+        <h3 class="font-bold text-2xl">Export</h3>
+        <p class="mt-4" v-if="username">
+          Will write
+          <code class="text-sm bg-gray-100 p-1 rounded-md">{{ fileName }}</code>
+          to
+          <code class="text-sm bg-gray-100 p-1 rounded-md">{{ username }}/{{ repoName }}</code>.
+        </p>
+        <div class="action mt-4">
+          <button
+            class="button text-blue-900 bg-blue-100 hover:bg-blue-200 focus:ring-blue-500"
+            :disabled="!username"
+            @click="writeToGitHub">
+            Export
+          </button>
+          <span
+            v-if="writeOutcome"
+            class="ml-2"
+            :class="writeOutcome.isSuccessful ? 'text-green-600' : 'text-red-600'">
+            {{ writeOutcome.message }}
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
+  import { mapGetters, mapMutations } from 'vuex'
   import { Octokit } from '@octokit/rest'
 
   import { Outcome } from '@/models/outcome'
+  import { AccountInterface } from '@/models/account'
+
+  import {
+    createNewBranch,
+    getBranchSha,
+    getDefaultBranch,
+    getFile,
+    updateFile,
+  } from '@/support/github'
 
   export default defineComponent({
     name: 'GitHub',
     data() {
       return {
-        outcome: null as Outcome<string> | null,
+        repoName: '2fa-secrets',
+        fileName: 'secrets.json',
+        octokit: null as Octokit | null,
         accessToken: '',
+        loginOutcome: null as Outcome<string> | null,
+        readOutcome: null as Outcome<AccountInterface[]> | null,
+        writeOutcome: null as Outcome<never> | null,
       }
     },
     watch: {
-      accessToken(toValue) {
-        localStorage.setItem('access_token', toValue)
+      accessToken(toValue, fromValue) {
+        if (toValue !== fromValue) {
+          this.octokit = null
+          this.loginOutcome = null
+          localStorage.setItem('access_token', toValue)
+        }
       },
+    },
+    computed: {
+      client(): Octokit {
+        return this.octokit ?? new Octokit({ auth: this.accessToken })
+      },
+      username(): string | undefined {
+        return this.loginOutcome?.data
+      },
+      ...mapGetters('twoFa', [
+        'accountsJson',
+      ]),
     },
     methods: {
       async validateToken() {
-        const octokit = new Octokit({ auth: this.accessToken })
+        const octokit = this.client
         try {
-          const { data: { login } } = await octokit.users.getAuthenticated()
-          this.outcome = {
+          const { data: { login: username } } = await octokit.users.getAuthenticated()
+          this.loginOutcome = {
             isSuccessful: true,
-            message: `Hello, ${login}!`,
-            data: login,
+            message: `Hello, ${username}!`,
+            data: username,
           }
         } catch (ex) {
-          this.outcome = {
+          this.loginOutcome = {
             isSuccessful: false,
             message: ex.message,
           }
         }
       },
+      async readFromGitHub() {
+        if (!this.username) {
+          return
+        }
+
+        const octokit = this.client
+        let outcome: Outcome<AccountInterface[]>
+        try {
+          const defaultBranch = await getDefaultBranch(octokit, this.username, this.repoName)
+          const file = await getFile(octokit, this.username, this.repoName, this.fileName, defaultBranch)
+
+          if (!file.content) {
+            throw new Error('Content missing')
+          }
+          const accPojos = JSON.parse(file.content)
+          this.clearAccounts()
+          this.loadAccounts({ accPojos })
+          outcome = {
+            isSuccessful: true,
+            message: 'Great success!',
+            data: accPojos,
+          }
+        } catch (ex) {
+          outcome = {
+            isSuccessful: false,
+            message: ex.message,
+          }
+        }
+        this.readOutcome = outcome
+      },
+      async writeToGitHub() {
+        if (!this.username) { return }
+
+        const octokit = this.client
+        let outcome: Outcome<never>
+        try {
+          const defaultBranch = await getDefaultBranch(octokit, this.username, this.repoName)
+          const branchSha = await getBranchSha(octokit, this.username, this.repoName, defaultBranch)
+          const newBranch = Date.now().toString()
+          await createNewBranch(octokit, this.username, this.repoName, newBranch, branchSha)
+          const file = await getFile(octokit, this.username, this.repoName, this.fileName, newBranch)
+          if (!file.sha) {
+            throw new Error('Missing SHA')
+          }
+          if (file.content === this.accountsJson) {
+            outcome = {
+              isSuccessful: true,
+              message: 'Contents are identical.',
+            }
+            this.writeOutcome = outcome
+            return
+          }
+          await updateFile(octokit, this.username, this.repoName, newBranch, this.fileName, this.accountsJson, file.sha)
+
+          outcome = {
+            isSuccessful: true,
+            message: `Pushed to branch ${newBranch}`,
+          }
+        } catch (ex) {
+          outcome = {
+            isSuccessful: false,
+            message: ex.message,
+          }
+        }
+        this.writeOutcome = outcome
+      },
+      ...mapMutations('twoFa', [
+        'clearAccounts',
+        'loadAccounts',
+      ]),
     },
     created() {
       this.accessToken = localStorage.getItem('access_token') ?? ''
+    },
+    beforeUnmount() {
+      localStorage.setItem('access_token', this.accessToken)
     },
   })
 </script>
---
+++ src/support/github.ts
@@ -0,0 +1,93 @@
+import { Octokit } from '@octokit/rest'
+
+interface File {
+  sha?: string
+  content?: string
+}
+
+export const getFile = async (
+  client: Octokit,
+  username: string,
+  repoName: string,
+  fileName: string,
+  refName: string,
+): Promise<File> => {
+  const { data } = await client.repos.getContent({
+    owner: username,
+    repo: repoName,
+    path: fileName,
+    ref: refName,
+  })
+
+  const file: File = {}
+  if ('sha' in data) {
+    const { sha } = data
+    file.sha = sha
+  }
+  if ('content' in data) {
+    const { content } = data
+    file.content = atob(content)
+  }
+  return file
+}
+
+export const updateFile = async (
+  client: Octokit,
+  username: string,
+  repoName: string,
+  branchName: string,
+  fileName: string,
+  content: string,
+  fileSha: string,
+): Promise<void> => {
+  await client.repos.createOrUpdateFileContents({
+    owner: username,
+    repo: repoName,
+    branch: branchName,
+    path: fileName,
+    content: btoa(content),
+    sha: fileSha,
+    message: 'Update 2FA tokens',
+  })
+}
+
+export const getDefaultBranch = async (
+  client: Octokit,
+  username: string,
+  repoName: string,
+): Promise<string> => {
+  const { data: { default_branch: defaultBranch } } = await client.repos.get({
+    owner: username,
+    repo: repoName,
+  })
+  return defaultBranch
+}
+
+export const getBranchSha = async (
+  client: Octokit,
+  username: string,
+  repoName: string,
+  branchName: string,
+): Promise<string> => {
+  const { data: { object: { sha } } } = await client.git.getRef({
+    owner: username,
+    repo: repoName,
+    ref: `heads/${branchName}`,
+  })
+  return sha
+}
+
+export const createNewBranch = async (
+  client: Octokit,
+  username: string,
+  repoName: string,
+  branchName: string,
+  sha: string,
+): Promise<void> => {
+  await client.git.createRef({
+    owner: username,
+    repo: repoName,
+    ref: `refs/heads/${branchName}`,
+    sha,
+  })
+}
