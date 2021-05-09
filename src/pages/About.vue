f083d587e37352a37bfb81ef1e1fb1dfa8da43c6
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-09 11:21:26 +0530

    Increase spacing and simplify classes

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="page p-2 my-4" id="about">
-    <div class="content max-w-screen-tl mx-auto">
-      <p>
-        Multifac is a TOTP generator. There are several multi-factor
-        authenticator applications out there. Why make another one?
-      </p>
+  <div class="page p-2 max-w-screen-tl mx-auto my-8" id="about">
+    <p>
+      Multifac is a TOTP generator. There are several multi-factor
+      authenticator applications out there. Why make another one?
+    </p>
 
-      <h2 class="font-bold text-3xl mt-4 mb-2">Unparalleled beauty</h2>
-      <p class="mt-2 mb-4">
-        Hardly any of the authenticator apps focus on aesthetics and user
-        experience. Multifac is different. It has been designed with utmost
-        attention to detail and an unwavering dedication to beautiful UI/UX.
-      </p>
+    <h2 class="font-bold text-3xl mt-4 mt-8">Unparalleled beauty</h2>
+    <p class="mt-4">
+      Hardly any of the authenticator apps focus on aesthetics and user
+      experience. Multifac is different. It has been designed with utmost
+      attention to detail and an unwavering dedication to beautiful UI/UX.
+    </p>
 
-      <h2 class="font-bold text-3xl mt-4 mb-2">Data ownership</h2>
-      <p class="mt-2 mb-4">
-        Authenticator apps tend to lock you in. Multifac does not need you to sign
-        up, nor does it keep any of your data. You always own your data and can
-        revoke our access whenever you want.
-      </p>
+    <h2 class="font-bold text-3xl mt-8">Data ownership</h2>
+    <p class="mt-4">
+      Authenticator apps tend to lock you in. Multifac does not need you to sign
+      up, nor does it keep any of your data. You always own your data and can
+      revoke our access whenever you want.
+    </p>
 
-      <h2 class="font-bold text-3xl mt-4 mb-2">Unrestricted access</h2>
-      <p class="mt-2 mb-4">
-        Few authenticator apps support multi-device, and those that do are
-        cumbersome. Multifac is built on web technologies and supports as many
-        devices as you have. There's no device limit of any kind.
-      </p>
-    </div>
+    <h2 class="font-bold text-3xl mt-8">Unrestricted access</h2>
+    <p class="mt-4">
+      Few authenticator apps support multi-device, and those that do are
+      cumbersome. Multifac is built on web technologies and supports as many
+      devices as you have. There's no device limit of any kind.
+    </p>
   </div>
 </template>
