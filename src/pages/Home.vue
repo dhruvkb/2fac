cbb80c8255e6d91b98f7b0945f6b203404f6ebc7
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-10 21:40:53 +0530

    Reorganise component hierarchy

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="page" id="home">
-    <template v-if="accounts.length">
-      <Timeline/>
-      <div class="grid gap-2 tp:gap-4 grid-cols-2 tl:grid-cols-3 dr:grid-cols-4 py-2 tp:px-2 my-8">
-        <Card
-          v-for="(acc,index) in accounts"
-          :key="index"
-          :account="acc"
-          :is-active="index === 5"/>
-      </div>
-    </template>
-    <template v-else>
-      <div class="text-center p-2 my-8">
-        It's a little lonely in here.
-      </div>
-    </template>
+    <Timeline v-if="accounts.length"/>
+    <div v-else class="text-center p-2 mt-8">
+      It's a little lonely in here.
+    </div>
+
+    <div class="grid gap-2 tp:gap-4 grid-cols-2 tl:grid-cols-3 dr:grid-cols-4 py-2 mt-8">
+      <Card
+        v-for="(acc,index) in accounts"
+        :key="index"
+        :account="acc"
+        :is-active="index === 5"/>
+    </div>
   </div>
 </template>
 
