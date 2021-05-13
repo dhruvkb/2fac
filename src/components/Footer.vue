efbfbdc8058323eba0931c8fc457711727dc6bac
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-13 05:58:34 +0530

    Integration component `Navigation` into the header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
-  <footer class="flex flex-col items-center tp:flex-row text-sm border-t border-gray-200">
-    <div class="text-gray-600 p-2">
+  <footer class="flex items-center tp:flex-row text-sm">
+    <div class="hidden tp:block text-gray-600 p-2">
       An experiment by
       <a
         class="font-bold text-black hover:underline"
         href="https://dhruvkb.github.io/">
         @dhruvkb</a>.
     </div>
+    <Navigation class="grid tp:hidden w-full"/>
   </footer>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Navigation from '@/components/Navigation.vue'
+
+  export default defineComponent({
+    name: 'Footer',
+    components: {
+      Navigation,
+    },
+  })
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
-  <header class="flex flex-col items-center tp:flex-row border-b border-gray-200">
-    <div class="site-title p-2 tp:mr-auto">
-      <h1 class="flex items-center font-bold text-2xl">
-        <ion-icon :src="logo"/>
+  <header class="flex">
+    <div class="site-title mr-auto">
+      <h1 class="flex items-center font-bold text-lg py-1">
         <RouterLink
           class="ml-1"
           :to="{ name: 'home'}">
@@ -10,40 +9,20 @@
         </RouterLink>
       </h1>
     </div>
-    <nav class="grid grid-cols-2 self-stretch">
-      <RouterLink
-        v-for="(link, index) in links"
-        :key="index"
-        class="flex items-end justify-center px-2 border-b-3 border-transparent transform translate-y-2px"
-        :to="{ name: link.toName }"
-        active-class="border-black">
-        <div class="pb-2">{{ link.text }}</div>
-      </RouterLink>
-    </nav>
+
+    <Navigation class="hidden tp:grid"/>
   </header>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
 
-  import logo from '@/assets/logo.svg'
+  import Navigation from '@/components/Navigation.vue'
 
   export default defineComponent({
     name: 'Header',
-    data() {
-      return {
-        logo,
-        links: [
-          {
-            text: 'Data',
-            toName: 'data',
-          },
-          {
-            text: 'About',
-            toName: 'about',
-          },
-        ],
-      }
+    components: {
+      Navigation,
     },
   })
 </script>
