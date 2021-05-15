8716aebd1e82ac5eb2fa498bf4660c54960711e8
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-16 02:00:16 +0530

    Add a helper function to fetch icons from the 2Fac API

---
+++ src/support/simple_icons.ts
@@ -0,0 +1,22 @@
+import { IconSvg } from '@/models/icon_svg'
+
+const API_ROOT = 'https://2fac-api.vercel.app/api/icons'
+
+/**
+ * Get the SVG path and hex color of the given icons from the 2Fac API hosted
+ * on Vercel. This is the one and only use of the API.
+ *
+ * @param {string} iconName - the name of the icon to fetch
+ * @return {IconSvg | null} the path and hex of the icon, or null if not found
+ */
+export const getIcon = async (iconName: string): Promise<IconSvg | null> => {
+  const res = await fetch([API_ROOT, iconName].join('/'))
+  const data = await res.json()
+  if (res.ok) {
+    return {
+      color: data.color,
+      path: data.path,
+    } as IconSvg
+  }
+  return null
+}
