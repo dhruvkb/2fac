be6ac14e068980a74ca6a6f47ac9cc4ddf4c47e4
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-22 06:17:31 +0530

    Sort exported accounts alphabetically

--- src/store/modules/two_fa/getters.ts
+++ src/store/modules/two_fa/getters.ts
@@ -10,7 +10,16 @@ export interface TwoFaGetterTree<S, RS> extends GetterTree<S, RS> {
 export const getters: TwoFaGetterTree<TwoFaState, RootState> = {
   accountsJson(state: TwoFaState): string {
     return JSON.stringify(
-      state.accounts,
+      state.accounts.sort((a, b) => {
+        let diff = 0
+        if (a.site && b.site) {
+          diff = a.site.localeCompare(b.site)
+        }
+        if (!diff && a.username && b.username) {
+          diff = a.username.localeCompare(b.username)
+        }
+        return diff
+      }),
       null,
       2, // spaces for indentation
     )
