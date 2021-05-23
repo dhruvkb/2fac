e25d37006bab4144c48ee988f8df18eba8ef8ae4
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-23 17:27:13 +0530

    Update the App layout interface

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
-  <div id="app-root" class="flex flex-col min-h-screen bg-gray-50">
-    <div class="wrapper top-0 border-b">
+  <div id="app-root" class="flex flex-col bg-gl-5 dark:bg-gd-6 min-h-screen">
+    <div
+      class="bar top-0 border-b transition-all duration-300"
+      :class="headerClasses">
       <Header class="w-full"/>
     </div>
     <div class="content-root flex-grow">
       <RouterView/>
     </div>
-    <div class="wrapper tp:relative bottom-0 border-t">
+    <div class="bar tp:relative bottom-0 border-t">
       <Footer class="w-full"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import {
+    computed,
+    defineComponent,
+  } from 'vue'
 
   import Header from '@/components/Header.vue'
   import Footer from '@/components/Footer.vue'
 
-  import { addListener, setBreakpoint } from '@/plugins/responsive'
+  import {
+    addListener,
+    setBreakpoint,
+  } from '@/plugins/responsive'
+  import { useStore } from 'vuex'
 
   export default defineComponent({
     components: {
@@ -26,8 +35,21 @@
       Footer,
     },
     setup() {
+      const store = useStore()
+
       setBreakpoint()
       addListener()
+
+      const isHeadingIntersecting = computed(() => store.state.ui.isHeadingIntersecting)
+      const headerClasses = computed(() => [
+        ...isHeadingIntersecting.value
+          ? ['dark:border-tx']
+          : ['border-sep-l', 'dark:border-sep-d'],
+      ])
+
+      return {
+        headerClasses,
+      }
     },
   })
 </script>
