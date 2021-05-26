4e8999b2dd5b5a941776ec1ab8478975573d91ce
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-26 19:09:49 +0530

    Change header style based on heading intersection with viewport

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,29 @@
 <template>
-  <header class="flex justify-between pt-2-safe pb-2 px-4-safe">
-    <div class="site-title">
-      <h1 class="flex items-center font-bold text-lg py-1">
-        <RouterLink :to="{ name: 'tokens'}">2Fac</RouterLink>
+  <header class="flex items-center justify-between pt-2-safe pb-2 px-4-safe h-12">
+    <!-- Teleportation destination -->
+    <div class="tp:hidden action-space left flex items-center justify-center"/>
+
+    <div
+      class="site-title transition-opacity duration-300"
+      :class="[...isHeadingIntersecting ? ['opacity-0'] : ['opacity-1']]">
+      <h1 class="flex items-center font-medium">
+        {{ currentPage }}
       </h1>
     </div>
 
     <!-- Teleportation destination -->
-    <div class="action-space flex items-center justify-center"/>
+    <div class="tp:hidden action-space right flex items-center justify-center"/>
 
     <Navigation class="hidden tp:grid"/>
   </header>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import {
+    computed,
+    defineComponent,
+  } from 'vue'
+  import { useStore } from 'vuex'
 
   import Navigation from '@/components/Navigation.vue'
 
@@ -22,6 +31,16 @@
     name: 'Header',
     components: {
       Navigation,
+    },
+    setup() {
+      const store = useStore()
+      const currentPage = computed(() => store.state.ui.currentPage)
+      const isHeadingIntersecting = computed(() => store.state.ui.isHeadingIntersecting)
+
+      return {
+        currentPage,
+        isHeadingIntersecting,
+      }
     },
   })
 </script>
