3cc8b5424ffe2a0dbb2f06a1296d5ff6b2af4fe6
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-26 19:11:08 +0530

    Imitate iOS look and feel in navigation element

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,19 @@
 <template>
-  <nav class="grid grid-cols-3 -space-x-px">
+  <nav class="grid grid-cols-3 -space-x-px text-ll-2 dark:text-ld-2">
     <RouterLink
       v-for="(link, index) in links"
       :key="index"
-      class="flex items-center justify-center gap-2 p-2 transition-colors border-gray-200"
-      :class="{ 'border-l': index !== 0 }"
+      class="link flex items-center justify-center gap-2 p-2 transition-colors duration-300"
       :to="{ name: link.toName }"
-      active-class="text-blue-600">
-      <Icon
-        class="h-5 w-5"
-        :name="link.iconName"
-        :src="link.iconSrc"/>
+      active-class="active text-bl dark:text-bd">
+      <div class="relative h-5 w-5">
+        <Icon
+          class="icon inactive absolute inset-0 transition-opacity duration-300"
+          :name="link.iconName"/>
+        <Icon
+          class="icon active absolute inset-0 transition-opacity duration-300"
+          :name="link.activeIconName"/>
+      </div>
       <span class="text-sm font-medium">{{ link.text }}</span>
     </RouterLink>
   </nav>
@@ -32,16 +35,19 @@
           {
             text: 'Tokens',
             iconName: 'shield-check',
+            activeIconName: 'shield-fill-check',
             toName: 'tokens',
           },
           {
             text: 'Data',
             iconName: 'hdd-stack',
+            activeIconName: 'hdd-stack-fill',
             toName: 'data',
           },
           {
             text: 'About',
             iconName: 'info-circle',
+            activeIconName: 'info-circle-fill',
             toName: 'about',
           },
         ],
@@ -49,3 +55,10 @@
     },
   })
 </script>
+
+<style scoped lang="css">
+  .link.active .icon.inactive,
+  .link:not(.active) .icon.active {
+    @apply opacity-0;
+  }
+</style>
