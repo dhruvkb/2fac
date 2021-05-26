d28b752fcaedd28275e6456361003765acb3419d
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-26 19:23:53 +0530

    Use new header and update disclaimer to occupy full-width on mobile phones

--- src/pages/Data.vue
+++ src/pages/Data.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="page max-w-screen-tl px-4-safe mx-auto my-8" id="data">
-    <h2 class="font-bold text-3xl">Data</h2>
+  <div class="max-w-screen-tl mx-auto px-4-safe mt-4">
+    <h2
+      class="font-bold text-3xl"
+      :class="headerClasses"
+      ref="marker">
+      Data
+    </h2>
+  </div>
 
-    <Disclaimer class="mt-4"/>
+  <div class="page max-w-screen-tl mx-auto mt-4 mb-8" id="data">
+    <div class="tp:px-4-safe">
+      <Disclaimer class="mt-4"/>
+    </div>
 
-    <p class="mt-4">
-      There's are two ways you can manage your data. You can manage your 2FA
-      secrets yourself or you can integrate 2Fac with GitHub. Both
-      approaches ensure that you have complete ownership and control over your
-      sensitive data.
-    </p>
+    <div class="px-4-safe">
+      <p class="mt-4">
+        There's are two ways you can manage your data. You can manage your 2FA
+        secrets yourself or you can integrate 2Fac with GitHub. Both
+        approaches ensure that you have complete ownership and control over your
+        sensitive data.
+      </p>
 
-    <File class="mt-8"/>
-    <GitHub class="mt-8"/>
-    <Delete class="mt-8"/>
+      <File class="mt-8"/>
+      <GitHub class="mt-8"/>
+      <Delete class="mt-8"/>
+    </div>
   </div>
 </template>
 
@@ -25,6 +36,9 @@
   import File from '@/data/File.vue'
   import Delete from '@/data/Delete.vue'
 
+  import { intersection } from '@/plugins/intersection'
+  import { useStore } from 'vuex'
+
   export default defineComponent({
     name: 'Data',
     components: {
@@ -32,6 +46,17 @@
       Disclaimer,
       GitHub,
       File,
+    },
+    setup() {
+      const store = useStore()
+      store.commit('ui/setCurrentPage', { currentPage: 'Data' })
+
+      const { marker, headerClasses } = intersection()
+
+      return {
+        marker,
+        headerClasses,
+      }
     },
   })
 </script>
