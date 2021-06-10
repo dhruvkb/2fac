2bd9488dc1ef4722e298f0a5a274f0a6ab279f5d
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-10 14:23:42 +0530

    Create a placeholder 'Settings' page

---
+++ src/components/Placeholder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="absolute inset-x-0 top-1/2 text-center transform -translate-y-1/2">
+    <!-- @slot Content goes here -->
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Placeholder',
+  })
+</script>
---
+++ src/pages/Settings.vue
@@ -0,0 +1,50 @@
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Settings</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent fullscreen>
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <Placeholder>
+        🚧 Work in progress...
+      </Placeholder>
+    </IonContent>
+  </IonPage>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  } from '@ionic/vue'
+
+  import Placeholder from '@/components/Placeholder.vue'
+
+  const ionicComponents = {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  }
+  export default defineComponent({
+    name: 'Tab2',
+    components: {
+      ...ionicComponents,
+      Placeholder,
+    },
+  })
+</script>
