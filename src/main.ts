8fb9bdec1d257691fa19cf19cfea6cc5fa761d20
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-10 07:19:39 +0530

    Setup Ionic with the Vue app

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import { createApp } from 'vue'
 
+import { IonicVue } from '@ionic/vue'
+
 import '@/registerServiceWorker'
 import router from '@/router'
 import store from '@/store'
 
 import App from '@/App.vue'
 
-import '@/styles/index.css' // Tailwind
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css'
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/float-elements.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/display.css'
+
+/* Theme variables */
+import '@/style/variables.css'
+import '@/style/variables_dark.css'
+import '@/style/theming_ios.css'
+import '@/style/theming_md.css'
+
+/* Tailwind */
+import '@/style/tailwind.css'
 
 const app = createApp(App)
 
 app
+  .use(IonicVue)
   .use(router)
   .use(store)
-  .mount('div#app__slot')
+
+router.isReady().then(() => {
+  app.mount('div#app__slot')
+})
