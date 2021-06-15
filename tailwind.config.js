62b8c5ac3ba188b995cb4f65ab08465f6fe129e0
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-15 11:25:35 +0530

    Add commonly used colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
+      colors: {
+        brand: 'var(--ion-color-brand)',
+        primary: 'var(--ion-color-primary)',
+        success: 'var(--ion-color-success)',
+        warning: 'var(--ion-color-warning)',
+        danger: 'var(--ion-color-danger)',
+      },
+      textColor: {
+        1: 'var(--l-1)',
+        2: 'var(--l-2)',
+        3: 'var(--l-3)',
+      },
       minWidth: {
         14: '3.5rem',
       },
