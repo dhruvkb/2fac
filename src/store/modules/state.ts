acb6f264efbecc18b4a119c47e267136ed85fd29
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-18 01:06:01 +0530

    Remove redundant commas

--- src/store/modules/state.ts
+++ src/store/modules/state.ts
@@ -2,6 +2,6 @@ import { TwoFaState } from '@/store/modules/two_fa/state'
 import { UiState } from '@/store/modules/ui/state'
 
 export interface RootState {
-  twoFa: TwoFaState,
-  ui: UiState,
+  twoFa: TwoFaState
+  ui: UiState
 }
